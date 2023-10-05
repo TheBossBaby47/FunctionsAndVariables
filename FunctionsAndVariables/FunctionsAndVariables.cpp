5b97c48f2b654d438353b967a47a6aa7b4beb140
TheBossBaby47 <c3051363@newcastle.ac.uk>
2023-10-05 11:24:28 +0100

    Reduced function by changing while loop condition, but it may cause unpredectible behvaiour in few cases so I suggest to not use it, in second approach I am using ternary operator and passing retrun value of assignment operator operator

--- FunctionsAndVariables/FunctionsAndVariables.cpp
+++ FunctionsAndVariables/FunctionsAndVariables.cpp
@@ -3,8 +3,32 @@ namespace NCL
 {
     void v1(char* a, const char* b) {
         while (*b != '\0')
-            *a++ = *b++;
+        {
+            *a = *b;
+            a++;
+            b++;
+        }
         *a = '\0';
+    }
+    /// <summary>
+    /// Copies the source char array to destination char array
+    /// </summary>
+    /// <param name="InDestination"> Pointer to first element of destination char array. </param>
+    /// <param name="InSource"> Pointer to first element of source char array.</param>
+    void StringCopy(char* InDestination, const char* InSource)
+    {
+        while (*InSource - 1 != '\0')
+            *InDestination++ = *InSource++;
+    }
+
+    /// <summary>
+    /// Copies the source char array to destinatio char array, uses ternary operator and return value of assignment operator
+    /// </summary>
+    /// <param name="InDestination"> Pointer to first element of destination char array. </param>
+    /// <param name="InSource"> Pointer to first element of source char array.</param>
+    void CharArrayCopy(char* InDestination, const char* InSource)
+    {
+        while ((*InSource != '\0') ? *InDestination++ = *InSource++ : *InDestination = '\0'){}
     }
 
     void PrintCharArray(const char* InArray)
@@ -21,6 +45,8 @@ int main()
     char a[5];
     
     NCL::PrintCharArray(b);
-    NCL::v1(a, b);
+    NCL::CharArrayCopy(a, b);
     NCL::PrintCharArray(a);
+
+    return 0;
 }
